46b7abca377361bfe5f49033fe9f757461afdf33
honzamucha <honza@softbanka.cz>
2026-02-10 09:17:08 +0100

    Přidání konstruktoru a atributů zvířete. Přepracování přidání zvířete.

--- KonzoleUI.cs
+++ KonzoleUI.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,29 +40,86 @@ namespace yvirata
 
         private void Pridat()
         {
-            Console.Write("Jméno: ");
-            string jmeno = Console.ReadLine();
-
-            Console.Write("Druh: ");
-            string druh = Console.ReadLine();
-
+            string jmeno;
+            string druh;
             int vek;
-            while (true)
+            string pohlavi;
+            string zdravotniStav;
+            string poznamka;
+
+            do
+            {
+                Console.Write("Jméno: ");
+                jmeno = Console.ReadLine();
+                
+                if (jmeno == "")
+                {
+                    Console.WriteLine("Toto pole je povinné.");
+                }
+            }
+            while (jmeno == "");
+
+
+            do
+            {
+                Console.Write("Druh: ");
+                druh = Console.ReadLine();
+
+                if (druh == "")
+                {
+                    Console.WriteLine("Toto pole je povinné.");
+                }
+            }
+            while (druh == "");
+
+            do
             {
                 Console.Write("Věk: ");
-                if (int.TryParse(Console.ReadLine(), out vek) && vek >= 0)
-                    break;
-            }
+                vek = int.Parse(Console.ReadLine());
 
-            evidence.Pridat(jmeno, druh, vek);
+                if (vek < 0)
+                {
+                    Console.WriteLine("Věk nemůže být záporný");
+                }
+            }
+            while (vek < 0);
+
+            do
+            {
+                Console.Write("Pohlaví: ");
+                pohlavi = Console.ReadLine();
+
+                if (pohlavi == "")
+                {
+                    Console.WriteLine("Toto pole je povinné.");
+                }
+            }
+            while (pohlavi == "");
+
+            do
+            {
+                Console.Write("Zdravotní stav: ");
+                zdravotniStav = Console.ReadLine();
+
+                if (zdravotniStav == "")
+                {
+                    Console.WriteLine("Toto pole je povinné.");
+                }
+            }
+            while (zdravotniStav == "");
+
+            Console.Write("Poznámka: ");
+            poznamka = Console.ReadLine();
+
+            evidence.Pridat(jmeno, druh, vek, pohlavi, zdravotniStav, poznamka);
         }
 
         private void Vypis()
         {
-            Console.WriteLine("\nID | Jméno | Druh | Věk | Adoptováno");
+            Console.WriteLine("\nID | Jméno | Druh | Věk | Adoptováno | Pohlaví | Zdravotní stav | Poznámka");
             foreach (Zvire z in evidence.Vsechna())
             {
-                Console.WriteLine($"{z.Id} | {z.Jmeno} | {z.Druh} | {z.Vek} | {(z.Adoptovano ? "ANO" : "NE")}");
+                Console.WriteLine($"{z.Id} | {z.Jmeno} | {z.Druh} | {z.Vek} | {(z.Adoptovano ? "ANO" : "NE")} | {z.Pohlavi} | {z.ZdravotniStav} | {z.Poznamka}");
             }
             Console.ReadKey();
         }
--- zvire.cs
+++ zvire.cs
@@ -14,14 +14,27 @@ namespace yvirata
         public string Druh { get; set; }
         public int Vek { get; set; }
         public bool Adoptovano { get; set; }
+        public string Pohlavi { get; set; }
+        public DateTime DatumPrijmu
+        {
+            get
+            {
+                return DateTime.Today;
+            }
+        }
+        public string ZdravotniStav { get; set; }
+        public string Poznamka { get; set; }
 
-        public Zvire(int id, string jmeno, string druh, int vek, bool adoptovano)
+        public Zvire(int id, string jmeno, string druh, int vek, bool adoptovano, string pohlavi, string zdravotniStav, string poznamka)
         {
             Id = id;
             Jmeno = jmeno;
             Druh = druh;
             Vek = vek;
             Adoptovano = adoptovano;
+            Pohlavi = pohlavi;
+            ZdravotniStav = zdravotniStav;
+            Poznamka = poznamka;
         }
     }
 
